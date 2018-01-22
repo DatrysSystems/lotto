8b298b4c0a6e2ccd1611dda7a87665f76659e8fc
MikeKellyDatrys <mike.kelly@datrysystems.com>
2018-01-22 15:39:44 +0000

    Code tidy up.

--- lotto.pl
+++ lotto.pl
@@ -17,19 +17,18 @@ for($l=1;$l<=$c;$l++){
         for($ar1=1;$ar1<=5;$ar1++){
                 do {
                         $rnd1=int(rand(50))+1;
-                        $rnd1="0".$rnd1 if($rnd1<10);
+                        $rnd1='0'.$rnd1 if($rnd1<10);
                 } until (!(grep(/^$rnd1$/,@arr1)));
                 $arr1[$ar1]=$rnd1;
         }
         for($ar2=1;$ar2<=2;$ar2++){
                 do {
                         $rnd2=int(rand(11))+1;
-                        $rnd2="0".$rnd2 if($rnd2<10);
+                        $rnd2='0'.$rnd2 if($rnd2<10);
                 } until (!(grep(/^$rnd2$/,@arr2)));
                 $arr2[$ar2]=$rnd2;
         }
         @arr1=sort{$a<=>$b}@arr1;
         @arr2=sort{$a<=>$b}@arr2;
-        print "@arr1  ";
-        print "@arr2 \n";
+        print "@arr1  @arr2\n";
 }
