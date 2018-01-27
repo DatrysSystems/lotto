cbfd99512c406d8f43de707dfb478a224d416b03
MikeKellyDatrys <mike.kelly@datrysystems.com>
2018-01-27 17:19:53 +0000

    Implemented strict refs

--- lotto.pl
+++ lotto.pl
@@ -1,4 +1,6 @@
-#!/usr/bin/perl
+#!perl
+
+use strict;
 
 #-------------------------------------------------------
 #
@@ -13,18 +15,21 @@
 #
 #--------------------------------------------------------
 
-$c=@ARGV[0];
-($val,$rnd)=0;
-(@arr1,@arr2)=undef;
-for($l=1;$l<=$c;$l++){
-        for($ar1=1;$ar1<=5;$ar1++){
+my $c=@ARGV[0];
+
+print "**** Euro Millions Generator ****\n";
+for(my $l=1;$l<=$c;$l++){
+        my (@arr1, @arr2)=undef;
+        for(my $ar1=1;$ar1<=5;$ar1++){
+                my ($rnd1);
                 do {
                         $rnd1=int(rand(50))+1;
                         $rnd1='0'.$rnd1 if($rnd1<10);
                 } until (!(grep(/^$rnd1$/,@arr1)));
                 $arr1[$ar1]=$rnd1;
         }
-        for($ar2=1;$ar2<=2;$ar2++){
+        for(my $ar2=1;$ar2<=2;$ar2++){
+                my ($rnd2);
                 do {
                         $rnd2=int(rand(11))+1;
                         $rnd2='0'.$rnd2 if($rnd2<10);
@@ -33,5 +38,6 @@ for($l=1;$l<=$c;$l++){
         }
         @arr1=sort{$a<=>$b}@arr1;
         @arr2=sort{$a<=>$b}@arr2;
-        print "@arr1  @arr2\n";
+        print "Line $l => @arr1  @arr2\n";
 }
+print "*********************************\n";
